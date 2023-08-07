341f408ba986bdead9e71ee7a649089c7f23e545
konark111 <136243900+konark111@users.noreply.github.com>
2023-08-07 15:33:49 +0530

    Update variables.tf

--- jenkins-terraform-pipeline/ec2_pipeline/variables.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/variables.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
   type = string
-  default = "AKIATW3G2VJNHIFZS2FD"
+  
 }
 variable "secret_key" {
   type = string
-  default = "VQh3DlTUiEYucB1597fXEQelYzBDuVANOOWIQcyZ"
+  
 }
